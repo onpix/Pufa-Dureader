147ad76481f3334df109086f1e40bf2d8c7fcc76
continue <362418096@qq.com>
2018-08-24 03:08:09 -0400

    ???

--- my_dureader_single/run.py
+++ my_dureader_single/run.py
@@ -95,7 +95,7 @@ def parse_args():
                                    '../data/data_test_preprocessed.json'],
                                help='list of files that contain the preprocessed test data')
     path_settings.add_argument('--pre_train_file', type=str,
-                               default='../data/mrc.vec', help='pre_train files')
+                               default='../data/mrcnew.vec', help='pre_train files')
     path_settings.add_argument('--vocab_dir', default='../data/vocab_search_pretrain/',
                                help='the dir to save vocabulary')
     path_settings.add_argument('--model_dir', default='../data/models_search_pretrain/',
