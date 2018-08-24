99d2a05c5d95b72ec8b9febe26e3ee4db374a3d8
continue <362418096@qq.com>
2018-08-24 12:07:33 -0400

    emmmm?

---
+++ data/yesno/yesno.py
@@ -0,0 +1,30 @@
+# -*- encoding: utf-8 -*-
+import json
+file_list = ['./data_dev_preprocessed.json', './data_test_preprocessed.json', './data_train_preprocessed.json']
+simbol_list = [ '，', '。']
+for file_name in file_list:
+    current_file = open(file_name+'.bak', 'r', encoding='utf-8')
+    new_file = open(file_name, 'w+', encoding='utf-8')
+    for line in current_file.readlines():
+        que_json = json.loads(line)
+        buf = que_json['documents'][0]['title'].replace('，','').replace('。', '')
+        que_json['documents'][0]['title'] = buf
+        buf = que_json['documents'][0]['paragraphs'][0].replace('，','').replace('。', '')
+        que_json['documents'][0]['paragraphs'][0] = json.loads(line)
+        buf = que_json['question'].replace('，','').replace('。', '')
+        que_json['question'] = buf
+        if que_json['answers']:
+            buf = que_json['answers'][0].replace('，','').replace('。', '')
+            que_json['answers'][0] = buf
+            buf = [x for x in que_json['segmented_answers'][0] if x not in simbol_list]
+            que_json['segmented_answers'][0] = buf
+        buf = [x for x in que_json['documents'][0]['segmented_title'] if x not in simbol_list]
+        que_json['documents'][0]['segmented_title'] = buf
+        buf = [x for x in que_json['documents'][0]['segmented_paragraphs'][0] if x not in simbol_list]
+        que_json['documents'][0]['segmented_paragraphs'][0] = buf
+        buf = [x for x in que_json['segmented_question'] if x not in simbol_list]
+        que_json['segmented_question'] = buf
+        new_line = json.dumps(que_json, ensure_ascii = False)
+        new_file.write(new_line+'\n')
+    current_file.close()
+    new_file.close()
--- my_dureader_single/train.sh
+++ my_dureader_single/train.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 export CUDA_VISIBLE_DEVICES=1
-<<<<<<< HEAD
-nohup python -u run.py 1>test_search.log 2>&1 &
-=======
-nohup python -u run.py 1>test_search.log 2>&1 &
->>>>>>> de2ea257e40573751b78dd10bff45ecd998de514
+nohup python -u run.py --epochs 30 --algo MLSTM 1>test_search.log 2>&1 &
